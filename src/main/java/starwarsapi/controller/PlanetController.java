795fd2de43db507c892a108d42cd6766142978ae
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-20 16:38:19 -0300

    Add GET /planets/:id resource to find planet by Id

--- src/main/java/starwarsapi/controller/PlanetController.java
+++ src/main/java/starwarsapi/controller/PlanetController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import starwarsapi.exception.PlanetNotFoundException;
 import starwarsapi.model.Planet;
 import starwarsapi.repository.PlanetRepository;
 
@@ -21,6 +22,12 @@ public class PlanetController {
     @ResponseStatus(HttpStatus.OK)
     public List<Planet> list() {
         return repo.findAll();
+    }
+
+    @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Planet findById(@PathVariable String id) {
+        return repo.findById(id).orElseThrow(PlanetNotFoundException::new);
     }
 
 }
