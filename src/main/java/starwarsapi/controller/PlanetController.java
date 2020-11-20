78a5d1b0a83c94a5613373c939b94b1791a184cb
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-20 16:36:51 -0300

    Add GET /planets resource for planets listing

--- src/main/java/starwarsapi/controller/PlanetController.java
+++ src/main/java/starwarsapi/controller/PlanetController.java
@@ -1,9 +1,26 @@
 package starwarsapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import starwarsapi.model.Planet;
+import starwarsapi.repository.PlanetRepository;
 
 @CrossOrigin
 @RequestMapping("planets")
 @RestController
 public class PlanetController {
+
+    @Autowired
+    private PlanetRepository repo;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Planet> list() {
+        return repo.findAll();
+    }
+
 }
