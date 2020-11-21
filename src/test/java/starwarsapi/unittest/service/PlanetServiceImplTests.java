526ab9b0a21a68b43f25c89212d6b6f43ce4bdbb
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:41:17 -0300

    Add create and list All tests in PlanetServiceImplTests

--- src/test/java/starwarsapi/unittest/service/PlanetServiceImplTests.java
+++ src/test/java/starwarsapi/unittest/service/PlanetServiceImplTests.java
@@ -3,6 +3,8 @@ package starwarsapi.unittest.service;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -13,12 +15,17 @@ import starwarsapi.filter.PlanetFilter;
 import starwarsapi.model.Planet;
 import starwarsapi.repository.PlanetRepository;
 import starwarsapi.service.PlanetServiceImpl;
+import starwarsapi.swapi.SwPlanet;
+import starwarsapi.swapi.SwPlanetService;
 
 @SpringBootTest
 public class PlanetServiceImplTests {
  
     @Mock
     private PlanetRepository planetRepo;
+
+    @Mock
+    private SwPlanetService swPlanetService;
 
     @InjectMocks
     private PlanetServiceImpl planetService;
@@ -38,4 +45,29 @@ public class PlanetServiceImplTests {
         assertSame(expected, actual);
     }
 
+    @Test
+    private void testListAll() {
+        var filter = new PlanetFilter();
+
+        Page<Planet> expected = Page.empty();
+
+        when(planetRepo.findAll(filter.pageable())).thenReturn(expected);
+
+        Page<Planet> actual = planetService.list(filter);
+
+        assertSame(expected, actual);
+    }
+
+    @Test
+    private void testCreate() {
+        Planet planet = new Planet().withName("Mars").withClimate("Cold").withTerrain("Desert");
+        SwPlanet swPlanet = new SwPlanet()
+            .withName(planet.getName())
+            .withFilms(List.of("The Martian"));
+
+        when(swPlanetService.findOneByName(planet.getName())).thenReturn(Optional.of(swPlanet));
+        when(planetRepo.insert(planet.withTotalFilmAppearances(swPlanet.getFilms().size())));
+
+        Planet actual = planetService.create(planet);
+    }
 }
