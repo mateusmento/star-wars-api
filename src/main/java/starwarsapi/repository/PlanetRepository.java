b2b1f0d1d62cefa554c34d7e0c19a560efd330cf
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:43:36 -0300

    Add findByNameIgnoreCaseContaining in PlanetRepository

--- src/main/java/starwarsapi/repository/PlanetRepository.java
+++ src/main/java/starwarsapi/repository/PlanetRepository.java
@@ -1,8 +1,11 @@
 package starwarsapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import starwarsapi.model.Planet;
 
 public interface PlanetRepository extends MongoRepository<Planet, String> {
+    Page<Planet> findByNameIgnoreCaseContaining(String name, Pageable pageable);
 }
