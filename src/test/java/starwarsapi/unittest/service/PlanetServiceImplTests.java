e8e4dfa63630a934bc949df2a947663d6274f6d9
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:34:36 -0300

    Add PlanetServiceImpl listByName test

--- src/test/java/starwarsapi/unittest/service/PlanetServiceImplTests.java
+++ src/test/java/starwarsapi/unittest/service/PlanetServiceImplTests.java
@@ -1,8 +1,41 @@
 package starwarsapi.unittest.service;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import starwarsapi.filter.PlanetFilter;
+import starwarsapi.model.Planet;
+import starwarsapi.repository.PlanetRepository;
+import starwarsapi.service.PlanetServiceImpl;
 
 @SpringBootTest
 public class PlanetServiceImplTests {
-    
+ 
+    @Mock
+    private PlanetRepository planetRepo;
+
+    @InjectMocks
+    private PlanetServiceImpl planetService;
+
+    @Test
+    private void listByName() {
+        var filter = new PlanetFilter();
+        filter.setName("Planet 1");
+
+        Page<Planet> expected = Page.empty();
+
+        when(planetRepo.findByNameIgnoreCaseContaining(filter.getName(), filter.pageable()))
+            .thenReturn(expected);
+
+        Page<Planet> actual = planetService.list(filter);
+
+        assertSame(expected, actual);
+    }
+
 }
