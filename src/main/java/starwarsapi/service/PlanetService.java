8f3b7571abd43240c7b5fd1af327f6c15d32e3f0
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:41:51 -0300

    Add methods to PlanetService

--- src/main/java/starwarsapi/service/PlanetService.java
+++ src/main/java/starwarsapi/service/PlanetService.java
@@ -1,4 +1,13 @@
 package starwarsapi.service;
 
+import org.springframework.data.domain.Page;
+
+import starwarsapi.filter.PlanetFilter;
+import starwarsapi.model.Planet;
+
 public interface PlanetService {
+    Page<Planet> list(PlanetFilter filter);
+    Planet findById(String id);
+    Planet create(Planet planet);
+    Planet delete(String id);
 }
