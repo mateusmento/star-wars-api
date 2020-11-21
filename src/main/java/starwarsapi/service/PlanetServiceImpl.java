5ff0429f34221075833c6f7aacbe98162d75d3ae
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:40:32 -0300

    Add list method in PlanetServiceImpl

--- src/main/java/starwarsapi/service/PlanetServiceImpl.java
+++ src/main/java/starwarsapi/service/PlanetServiceImpl.java
@@ -1,6 +1,21 @@
 package starwarsapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import starwarsapi.filter.PlanetFilter;
+import starwarsapi.model.Planet;
+import starwarsapi.repository.PlanetRepository;
 @Service
 public class PlanetServiceImpl implements PlanetService {
 
+    @Autowired
+    private PlanetRepository planetRepo;
+
+    public Page<Planet> list(PlanetFilter filter) {
+        var pageable = filter.pageable();
+        if (filter.getName() != null)
+            return planetRepo.findByNameIgnoreCaseContaining(filter.getName(), pageable);
+        return planetRepo.findAll(pageable);
+    }
 }
