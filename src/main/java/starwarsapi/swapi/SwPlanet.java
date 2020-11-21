b56209efa6f7bf9df66cbcbc801026138a6705c8
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:43:50 -0300

    Add lombok annotations in SwPlanet

--- src/main/java/starwarsapi/swapi/SwPlanet.java
+++ src/main/java/starwarsapi/swapi/SwPlanet.java
@@ -2,9 +2,17 @@ package starwarsapi.swapi;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.With;
 
+@With
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class SwPlanet {
     private String name;
     private List<String> films;
