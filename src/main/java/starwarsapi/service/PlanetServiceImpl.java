31acf3bfdc0685c5e583a4ac15da3dc44db353fa
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:42:52 -0300

    Implement methods in PlanetServiceImpl

--- src/main/java/starwarsapi/service/PlanetServiceImpl.java
+++ src/main/java/starwarsapi/service/PlanetServiceImpl.java
@@ -3,11 +3,18 @@ package starwarsapi.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import starwarsapi.exception.PlanetNotFoundException;
 import starwarsapi.filter.PlanetFilter;
 import starwarsapi.model.Planet;
 import starwarsapi.repository.PlanetRepository;
+import starwarsapi.swapi.SwPlanetService;
+
 @Service
 public class PlanetServiceImpl implements PlanetService {
+
+    @Autowired
+    private SwPlanetService swPlanetService;
 
     @Autowired
     private PlanetRepository planetRepo;
@@ -17,5 +24,22 @@ public class PlanetServiceImpl implements PlanetService {
         if (filter.getName() != null)
             return planetRepo.findByNameIgnoreCaseContaining(filter.getName(), pageable);
         return planetRepo.findAll(pageable);
+    }
+
+    public Planet findById(String id) {
+        return planetRepo.findById(id).orElseThrow(PlanetNotFoundException::new);
+    }
+
+    public Planet create(Planet planet) {
+        var swPlanet = swPlanetService.findOneByName(planet.getName());
+        var appearances = swPlanet.map(p -> p.getFilms().size()).orElse(0);
+        Planet insertable = planet.withTotalFilmAppearances(appearances);
+        return planetRepo.insert(insertable);
+    }
+
+    public void delete(String id) {
+        if (planetRepo.existsById(id))
+            throw new PlanetNotFoundException();
+        planetRepo.deleteById(id);
     }
 }
