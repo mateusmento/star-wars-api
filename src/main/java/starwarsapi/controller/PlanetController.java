e518b6641f964c95014b6c5b9b5f293389fa7730
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:45:29 -0300

    Add create and delete resource for Planet

--- src/main/java/starwarsapi/controller/PlanetController.java
+++ src/main/java/starwarsapi/controller/PlanetController.java
@@ -1,14 +1,13 @@
 package starwarsapi.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import starwarsapi.exception.PlanetNotFoundException;
+import starwarsapi.filter.PlanetFilter;
 import starwarsapi.model.Planet;
-import starwarsapi.repository.PlanetRepository;
+import starwarsapi.service.PlanetServiceImpl;
 
 @CrossOrigin
 @RequestMapping("planets")
@@ -27,7 +26,15 @@ public class PlanetController {
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
     public Planet findById(@PathVariable String id) {
-        return repo.findById(id).orElseThrow(PlanetNotFoundException::new);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Planet create(@RequestBody Planet planet) {
+        return service.create(planet);
     }
 
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable String id) {
+        service.delete(id);
+    }
 }
