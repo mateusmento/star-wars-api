2f6edf3b0e52557b761d90e2e89db73491b546a8
Mateus Sarmento <mateussarmento@blumar.com.br>
2020-11-21 14:32:07 -0300

    Add SwPlanetPage search test

--- src/main/java/starwarsapi/swapi/SwPlanetPage.java
+++ src/main/java/starwarsapi/swapi/SwPlanetPage.java
@@ -1,11 +1,25 @@
 package starwarsapi.swapi;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SwPlanetPage {
-    private Integer count;
-    private List<SwPlanet> results;
+    private Integer count = 0;
+    private List<SwPlanet> results = new ArrayList<>();
+
+    public Optional<SwPlanet> search(String name) {
+        return results.stream()
+            .filter(p -> p.getName().toLowerCase().equals(name.toLowerCase()))
+            .findAny();
+    }
 }
--- src/test/java/starwarsapi/unittest/swapi/SwPlanetPageTests.java
+++ src/test/java/starwarsapi/unittest/swapi/SwPlanetPageTests.java
@@ -1,8 +1,30 @@
 package starwarsapi.unittest.swapi;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import starwarsapi.swapi.SwPlanet;
+import starwarsapi.swapi.SwPlanetPage;
 
 @SpringBootTest
 public class SwPlanetPageTests {
 
+    @Test
+    public void search() {
+        var expected = new SwPlanet().withName("Planet 1");
+        List<SwPlanet> planets = List.of(expected, new SwPlanet().withName("Planet 2"));
+
+        SwPlanetPage page = SwPlanetPage.builder().results(planets).build();
+
+        Optional<SwPlanet> actual = page.search("Planet 1");
+
+        assertTrue(actual.isPresent());
+        assertSame(expected, actual.get());
+    }
 }
